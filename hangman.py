95c48aa796da1129e7aa6241b50315eb3a11bf9d
misterjrshittu@gmail.com <misterjrshittu@gmail.com>
2022-09-23 15:35:49 +0100

    hangman code added

---
+++ hangman.py
@@ -0,0 +1,108 @@
+from random import *
+
+player_score = 0
+computer_score = 0
+
+def hangedman(hangman):
+    graphic = [
+        """ 
+            +------+
+            |
+            |
+            |
+            |
+            |
+        ===============
+        """,
+        """
+            +------+
+            |      |
+            |      O
+            |
+            |
+            |
+        ===============
+        """,
+        """
+            +------+
+            |      |
+            |      O
+            |     -|-
+            |     / \
+            |
+        =============
+        """]
+    print(graphic[hangman])
+    return
+
+def start():
+    print("Let's play a game of Linux Hangman.")
+    while game():
+        pass
+    score()
+
+def game():
+    dictionary = ["gnu", "kernel", "linux", "mageia", "penguin", "ubuntu"]
+    word = choice(dictionary)
+    word_length = len(word)
+    clue = word_length * ["_"]
+    tries = 6
+    letters_tried = ""
+    guesses = 0
+    letters_right = 0
+    letters_wrong = 0 
+    global computer_score, player_score
+    
+    while (letters_wrong != tries) and ("".join(clue) != choice(dictionary)):
+        letter = guess_letter()
+        if len(letter) == 1 and letter.isalpha():
+            if letters_tried.find(letter) != -1:
+                print("You've already picked", letter)
+            else:
+                letters_tried = letters_tried + letter
+                first_index = word.find(letter)
+                if first_index == -1:
+                    letters_wrong += 1
+                    print("Sorry,", letter, "isn't what we're looking for.")
+                else:
+                    print("Congratulations,", letter, "is correct")
+                    for i in range(word_length):
+                        if letter == word[i]:
+                            clue[i] = letter
+        else: 
+            print("Choose another")
+
+        hangedman(letters_wrong)
+        print(" ".join(clue))
+        print("Guesses: ", letters_tried)
+        
+        if letters_wrong == tries:
+            print("Game Over.")
+            print("The word was",  word)
+            computer_score += 1
+            break
+        return play_again()
+    
+def guess_letter():
+        print()
+        letter = input("Take a guess at our mystery word:")
+        letter.strip()
+        letter.lower()
+        print()
+        return letter
+    
+def play_again():
+    answer = input("Would you like to play again? Y/N: ")
+    if answer in ("Y", "y", "Yes", "yes", "of course"):
+        return answer
+    else: 
+        print("Thank you for playing with us, see you next time!")
+            
+def score():
+    global player_score, computer_score
+    print("High Scores")
+    print("player: ", player_score)
+    print("computer: ", computer_score)
+        
+    if __name__ == "__main__":
+        start()
